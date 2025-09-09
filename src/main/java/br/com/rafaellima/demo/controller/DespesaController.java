96333e6ffd6fa3b9c1478e40590c04199ed5098e
Rafael Lima <rafaelclima.ti@gmail.com>
2025-09-08 21:14:16 -0300

    feat: criado endpoint para listar e criar despesa.

---
+++ src/main/java/br/com/rafaellima/demo/controller/DespesaController.java
@@ -0,0 +1,45 @@
+package br.com.rafaellima.demo.controller;
+
+import br.com.rafaellima.demo.dto.DespesaRequestDTO;
+import br.com.rafaellima.demo.dto.DespesaResponseDTO;
+import br.com.rafaellima.demo.model.Usuario;
+import br.com.rafaellima.demo.service.DespesaService;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+
+@RestController
+@RequestMapping("/api/despesas")
+@RequiredArgsConstructor
+public class DespesaController {
+   private final DespesaService despesaService;
+
+   @GetMapping
+   public ResponseEntity<Page<DespesaResponseDTO>> listarDespesas(@PageableDefault(page = 0, size = 10) Pageable paginacao, @AuthenticationPrincipal Usuario usuarioLogado) {
+      return ResponseEntity.ok(despesaService.listar(paginacao, usuarioLogado));
+   }
+
+   @PostMapping
+   public ResponseEntity<Void> criarDespesa(@RequestBody @Valid DespesaRequestDTO requestDTO,
+                                         @AuthenticationPrincipal Usuario usuarioLogado) {
+      var despesaCriada = despesaService.criar(requestDTO, usuarioLogado);
+
+      URI location =
+            ServletUriComponentsBuilder
+                  .fromCurrentRequest()
+                  .path("/{id}")
+                  .buildAndExpand(despesaCriada.getId())
+                  .toUri();
+      return ResponseEntity.created(location).build();
+   }
+}
---
+++ src/main/java/br/com/rafaellima/demo/service/DespesaService.java
@@ -0,0 +1,40 @@
+package br.com.rafaellima.demo.service;
+
+import br.com.rafaellima.demo.dto.DespesaRequestDTO;
+import br.com.rafaellima.demo.dto.DespesaResponseDTO;
+import br.com.rafaellima.demo.exception.UsuarioNaoAutenticadoException;
+import br.com.rafaellima.demo.model.Despesa;
+import br.com.rafaellima.demo.model.Usuario;
+import br.com.rafaellima.demo.repository.DespesasRepository;
+import br.com.rafaellima.demo.repository.UsuarioRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class DespesaService {
+   private final DespesasRepository despesasRepository;
+
+   public Page<DespesaResponseDTO> listar(Pageable paginacao, Usuario usuarioLogado) {
+      if (usuarioLogado == null) {
+         throw new UsuarioNaoAutenticadoException("Usuário não autenticado! Por favor, faça login.");
+      }
+      Long userId = usuarioLogado.getId();
+      return despesasRepository.findAllBy(userId, paginacao);
+   }
+
+   @Transactional
+   public Despesa criar(DespesaRequestDTO requestDTO, Usuario usuarioLogado) {
+      if (usuarioLogado == null) {
+         throw new UsuarioNaoAutenticadoException("Usuário não autenticado! Por favor, faça login");
+      }
+      var despesa = new Despesa(requestDTO, usuarioLogado);
+      despesasRepository.save(despesa);
+      return despesa;
+   }
+}
