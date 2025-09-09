b2eb497c0c59e195ada999614c822fa3ed256811
Rafael Lima <rafaelclima.ti@gmail.com>
2025-09-08 21:14:00 -0300

    feat: criado consulta paginada

--- src/main/java/br/com/rafaellima/demo/repository/DespesasRepository.java
+++ src/main/java/br/com/rafaellima/demo/repository/DespesasRepository.java
@@ -1,11 +1,27 @@
 package br.com.rafaellima.demo.repository;
 
+import br.com.rafaellima.demo.dto.DespesaResponseDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.com.rafaellima.demo.model.Despesa;
 
 @Repository
 public interface DespesasRepository extends JpaRepository<Despesa, Long> {
-
+   @Query("""
+          SELECT new br.com.rafaellima.demo.dto.ListarDespesasDTO(
+              d.id,
+              d.descricao,
+              d.valor,
+              d.dataVencimento,
+              d.status
+          ) FROM Despesa d
+          WHERE d.usuario.id = :userId
+          ORDER BY d.id DESC
+      """)
+   Page<DespesaResponseDTO> findAllBy(@Param("userId") Long userId, Pageable paginacao);
 }
